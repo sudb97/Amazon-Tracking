80ff3fbab5b53510a4fdb7cd33fd54eca5a391ba
ParnaSankarPan <71081945+ParnaSankarPan@users.noreply.github.com>
2021-06-07 19:40:50 +0530

    Upadated_sendmail.py

--- send_mail.py
+++ send_mail.py
@@ -1,4 +1,3 @@
-import smtplib
 from Google import Create_Service
 import base64
 from email.mime.multipart import MIMEMultipart
@@ -15,30 +14,33 @@ def send_thread(current_status,thd_id,msid,first):           #sending email repl
     service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
     if first==str(1):
-        emailMsg='Welcome to email thread for FitBit Charge 4 Amazon Price Tracker. Current price is:'+current_status+'\n'+'Visit https://www.amazon.in/Fitbit-Charge-Fitness-Tracker-Non-NFC/dp/B084CQ41M2/ref=sr_1_8?dchild=1&keywords=fitbit%2Bcharge%2B3&qid=1608032936&sr=8-8&th=1 for more details.'          #change the body here for first mail
+        emailMsg='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx'+current_status+'\n'+'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.'          #change the body here for first mail
         mimeMessage = MIMEMultipart()
-        mimeMessage['to'] = 'iotstreetlight@gmail.com'                                #change the recipient of the mail
-        mimeMessage['subject'] = 'FitBit Charge 4 Amazon Price Tracker'         #Change the subject for the 1st mail
+        mimeMessage['to'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'                                #change the recipient of the mail
+        mimeMessage['subject'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'         #Change the subject for the 1st mail
         mimeMessage.attach(MIMEText(emailMsg, 'plain'))
         raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
 
         message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
 
         return message["id"]
-    
+
     else:
         msg_id,ref_id=retrive_ids.get_mime_ids(msid)
-        message = MIMEText('There is an update: '+current_status+'\n'+'Please visit https://www.amazon.in/Fitbit-Charge-Fitness-Tracker-Non-NFC/dp/B084CQ41M2/ref=sr_1_8?dchild=1&keywords=fitbit%2Bcharge%2B3&qid=1608032936&sr=8-8&th=1 for more details.')  #change the body of the threaded mail
-        message['to'] = 'iotstreetlight@gmail.com'                                                #change the recipient of the thread mail
-        #message['from'] = 'parnasankarpan@gmail.com'                                              #It is redundant
-        message['subject'] = 'FitBit Charge 4 Amazon Price Tracker'                         #Change the subject of the threaded mail
-        message['In-Reply-To'] = msg_id       
-        message['References'] = ref_id       
+        message = MIMEText('xxxxxxxxxxxxxxxxxxxxxxxx'+current_status+'\n'+'Please visit https://www.amazon.in/Fitbit-Charge-Fitness-Tracker-Non-NFC/dp/B084CQ41M2/ref=sr_1_8?dchild=1&keywords=fitbit%2Bcharge%2B3&qid=1608032936&sr=8-8&th=1 for more details.')  #change the body of the threaded mail
+        message['to'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'                                                #change the recipient of the thread mail
+        message['subject'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'                         #Change the subject of the threaded mail
+        message['In-Reply-To'] = msg_id
+        message['References'] = ref_id
         sz_message = message.as_string()
         raw_msg_byte =base64.urlsafe_b64encode(six.ensure_binary(sz_message))
         raw_msg = six.ensure_str(raw_msg_byte)
         new_email = {'raw': raw_msg}
-        new_email['threadId'] = thd_id          
+        new_email['threadId'] = thd_id
 
         sent=service.users().messages().send(userId='me', body=new_email).execute()
-        return sent["id"]              
+        return sent["id"]
+
+
+
+    #Note : In place of XXXXXXXXXXXXXXXXXXXXXX   please writ according to your requirement
